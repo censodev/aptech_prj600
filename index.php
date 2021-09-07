b90d981eb89661eea7c93675d8b4bb8429a0541c
Phuong Bui <buiphuongaptutc@gmail.com>
2021-09-07 13:33:09 +0700

    refactor

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,40 +9,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
+
 <body>
-    <?php if (empty($_GET['url'])): ?>
-        <?php header('Location: home') ?>
-    <?php elseif (str_contains($_GET['url'], 'admin')): ?>
-        <!-- Start admin header -->
-        <?php include_once('./admin/layouts/header.php') ?>
-        <!-- End admin header -->
-        <div class="d-flex">
-            <?php include_once('./admin/layouts/sidebar.php') ?>
-            <div class="content">
-                <?php
-                    $path = './admin/pages/'.str_replace('admin/', '', $_GET['url']).'.php';
-                    if (file_exists($path)) {
-                        include_once($path);
-                    } else {
-                        include_once('./admin/pages/404.php');
-                    }
-                ?>
-            </div>
-        </div>
-        <!-- Start admin footer -->
-        <?php include_once('./admin/layouts/footer.php') ?>
-        <!-- End admin footer -->
-    <?php else: ?>
-        <?php include_once('./layouts/header.php') ?>
-        <?php
-            $path = './pages/'.$_GET['url'].'.php';
-            if (file_exists($path)) {
-                include_once($path);
-            } else {
-                include_once('./pages/404.php');
-            }
-        ?>
-        <?php include_once('./layouts/footer.php') ?>
-    <?php endif ?>
+    <?php
+    include_once('./helpers.php');
+    $url = $_GET['url'] ?? 'home';
+    $url = $url == 'admin' || $url == 'admin/' ? 'admin/home' : $url;
+    $path = './views/' . $url . '.php';
+    $layout_content = file_exists($path)
+        ? render($path)
+        : render('./views/404.php');
+    if (str_contains($path, 'admin')) {
+        include_once('./views/layouts/admin.php');
+    } else {
+        include_once('./views/layouts/default.php');
+    }
+    ?>
 </body>
+
 </html>
