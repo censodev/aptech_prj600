7605fc55913b221da8bb88c1c2946108b6720acb
Phuong Bui <buiphuongaptutc@gmail.com>
2021-10-10 14:47:23 +0700

    admin login

--- views/layouts/admin.view.php
+++ views/layouts/admin.view.php
@@ -26,12 +26,6 @@ $navbar = [
 
     <title>VMS | Admin</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-            crossorigin="anonymous"></script>
-
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <link href="<?php echo Helper::assets('css/app.css') ?>" rel="stylesheet">
