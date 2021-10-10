506961e2d266de2e8266882335a79f5cd645f88f
Phuong Bui <buiphuongaptutc@gmail.com>
2021-10-10 14:57:11 +0700

    otp login

--- core/api/login-otp.php
+++ core/api/login-otp.php
@@ -1,5 +1,13 @@
 <?php
 if (isset($_GET['phone'])) {
-    $_SESSION['phone'] = $_GET['phone'];
+    $phone = $_GET['phone'];
+    $user = new User();
+    $users = $user->findAll(['phone' => $phone]);
+    if (count($users) == 0) {
+        $user->insert([
+            'phone' => $phone,
+        ]);
+    }
+    $_SESSION['phone'] = $phone;
     echo json_encode(['msg' => 'Đăng nhập thành công']);
 }
