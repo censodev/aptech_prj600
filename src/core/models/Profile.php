0c07a87379680ab24c1a3739b1c3662e5ea5035e
Phuong Bui <buiphuongaptutc@gmail.com>
2021-10-11 10:21:15 +0700

    profile status

--- src/core/models/Profile.php
+++ src/core/models/Profile.php
@@ -7,3 +7,15 @@ class Profile extends Model
         return 'profiles';
     }
 }
+
+abstract class ProfileStatus
+{
+    const CREATED = 0;
+    const CHECKED_IN = 1;
+    const SCREENED = 2;
+    const INJECTED = 3;
+    const COMPLETED = 4;
+
+    const REJECTED = -1;
+    const FAILED = -2;
+}
