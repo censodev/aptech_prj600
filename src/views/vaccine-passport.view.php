01795f928a4bc69617d327461b59a9f688b22062
Phuong Bui <buiphuongaptutc@gmail.com>
2021-10-17 00:14:30 +0700

    sửa passport

--- src/views/vaccine-passport.view.php
+++ src/views/vaccine-passport.view.php
@@ -24,19 +24,19 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-3 infor">
+            <div class="col-md-3 infor">
               <label>Họ và tên:</label>
               <span id="name">Nguyễn Hải Đăng</span>
             </div>
-            <div class="col-3 infor">
+            <div class="col-md-3 infor">
               <label>Ngày sinh:</label>
               <span id="birthday">03/02/2000</span>
             </div>
-            <div class="col-3 infor">
-              <label>Số CMND/CCCD/HC:</label>
+            <div class="col-md-3 infor">
+              <label>CMND/CCCD/HC:</label>
               <span id="identification">0987654321</span>
             </div>
-            <div class="col-3 infor">
+            <div class="col-md-3 infor">
               <label>Số điện thoại:</label>
               <span id="phonenumber">0987654321</span>
             </div>
@@ -51,30 +51,32 @@
               <span id="address">Đã được tiêm vaccine phòng bệnh COVID-19</span>
             </div>
           </div>
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <td>Mũi số</td>
-                <td>Thời gian tiêm</td>
-                <td>Tên vaccine</td>
-                <td>Điểm tiêm</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>12/09/2021</td>
-                <td>COVID-19 - Vaccine AstraZeneca</td>
-                <td>Bệnh viện đa khoa Đan Phượng</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>23/10/2021</td>
-                <td>COVID-19 - Vaccine AstraZeneca</td>
-                <td>Bệnh viện đa khoa Đan Phượng</td>
-              </tr>
-            </tbody>
-          </table>
+            <div class="mw-100 overflow-auto">
+                <table class="table table-bordered mw-1">
+                    <thead>
+                    <tr>
+                        <td>Mũi số</td>
+                        <td>Thời gian tiêm</td>
+                        <td>Tên vaccine</td>
+                        <td>Điểm tiêm</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>12/09/2021</td>
+                        <td>COVID-19 - Vaccine AstraZeneca</td>
+                        <td>Bệnh viện đa khoa Đan Phượng</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>23/10/2021</td>
+                        <td>COVID-19 - Vaccine AstraZeneca</td>
+                        <td>Bệnh viện đa khoa Đan Phượng</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
       </div>
     </div>
