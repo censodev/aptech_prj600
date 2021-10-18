ce21b0e9b02bee65fd57467719c3c9d509e49630
Phuong Bui <buiphuongaptutc@gmail.com>
2021-10-18 22:53:07 +0700

    ...

--- src/views/layouts/admin.view.php
+++ src/views/layouts/admin.view.php
@@ -30,15 +30,9 @@ $navbar = [
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
-    <meta name="author" content="AdminKit">
-    <meta name="keywords"
-          content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="shortcut icon" href="<?php echo Helper::assets('img/icons/icon-48x48.png') ?>"/>
-
-    <link rel="canonical" href="https://demo-basic.adminkit.io/pages-blank.html"/>
+    <link rel="shortcut icon" href="<?php echo Helper::assets('img/9d8ed5_651b6cb038ff4917bcdbe0c58ca2c241_mv2.png') ?>"/>
 
     <title>VMS | Admin</title>
 
--- src/views/layouts/default.view.php
+++ src/views/layouts/default.view.php
@@ -8,12 +8,10 @@ Helper::authGuard();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VMS</title>
-    <link rel="shortcut icon" href="<?php echo Helper::assets('img/icons/icon-48x48.png') ?>" />
+    <link rel="shortcut icon" href="<?php echo Helper::assets('img/9d8ed5_651b6cb038ff4917bcdbe0c58ca2c241_mv2.png') ?>" />
     <link rel="stylesheet" href="<?php echo Helper::assets("css/layout.css") ?>"/>
-    <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
