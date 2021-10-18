8d09f983d47c6cb4f265f1ea206acb0f7a2cf992
Phuong Bui <buiphuongaptutc@gmail.com>
2021-10-18 19:25:20 +0700

    add injection_date to session

--- src/core/controllers/admin/home.controller.php
+++ src/core/controllers/admin/home.controller.php
@@ -2,14 +2,14 @@
 $injection_sites = (new InjectionSite())->findAll(['status' => 1]);
 
 $_GET['status'] = $_GET['status'] ?? 0;
-$_GET['date'] = $_GET['date'] ?? date('Y-m-d');
+$_SESSION['injection_date'] = $_GET['date'] ?? $_SESSION['injection_date'] ?? date('Y-m-d');
 $_SESSION['injection_site'] = $_GET['injection_site'] ?? $_SESSION['injection_site'] ?? $injection_sites[0]['id'] ?? '';
 
 // TODO: bổ sung query related
 $profiles = (new Profile())->findAll([
     'status' => $_GET['status'],
     'injection_site_id' => $_SESSION['injection_site'],
-    'injection_date' => $_GET['date'],
+    'injection_date' => $_SESSION['injection_date'],
 ]);
 
 include_once Helper::view('admin/home');
